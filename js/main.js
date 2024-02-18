54341214af449229e85083c0d01c2fe8073ae36e
Ollincatl <ollincatlj@gmail.com>
2024-02-17 19:37:33 -0600

    se agrego la sección de Proyectos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
+  
+  
+let btnDescargarCV = document.getElementById("btnDescargarCV");
+
+btnDescargarCV.addEventListener("click",function(event){
+  const rutaDescarga = document.createElement("a");
+  rutaDescarga.href = "./src/Documentos/cv_OllincatlJimenez.pdf"
+  rutaDescarga.download = "OllincatlJimenez_cv.pdf"
+
+  rutaDescarga.click();
+})
+  
+  
+  
   // Función para establecer la altura de la sección igual al alto de la pantalla del navegador
 function ajustarAlturaSeccion() {
     let alturaPantalla = window.innerHeight;
     let seccionInicio = document.getElementById('Inicio');
     let seccionSobreMi = document.getElementById('SobreMi');
     let seccionConocimientos = document.getElementById('Conocimientos');
+    let seccionPortafolio = document.getElementById("Portafolio");
+    let seccionContacto = document.getElementById("Contacto");
 
     seccionInicio.style.minHeight = alturaPantalla + 'px';
     seccionSobreMi.style.minHeight = alturaPantalla + 'px';
     seccionConocimientos.style.minHeight = alturaPantalla + 'px';
+    seccionPortafolio.style.minHeight = alturaPantalla + 'px';
+    seccionContacto.style.minHeight = alturaPantalla + 'px';
 
     console.log(alturaPantalla)
 }
